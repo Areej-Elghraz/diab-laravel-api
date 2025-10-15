ee29302ef50a6a124333c35e3999ae5b7c9762de
Areej-Elghraz <areegelgharaz@gmail.com>
2025-10-15 19:18:10 +0300

    Clean project from sensitive data

--- tests/Feature/ProductImageTest.php
+++ tests/Feature/ProductImageTest.php
@@ -8,35 +8,21 @@ beforeEach(function () {
         'images' => [
             UploadedFile::fake()->image('product1.jpg'),
             UploadedFile::fake()->image('product2.jpg'),
-            UploadedFile::fake()->image('product3.jpg'),
-            UploadedFile::fake()->image('product4.jpg'),
-            UploadedFile::fake()->image('product5.jpg'),
         ],
         'positions' => [
             'gallery',
             'gallery',
-            'after',
-            'gallery',
-            'before',
         ],
-        // 'image' => UploadedFile::fake()->image('product.jpg'),
-        // 'position' => fake()->randomElement(ProductImagePositionEnum::values())
+        'image' => UploadedFile::fake()->image('product.jpg'),
+        'position' => fake()->randomElement(ProductImagePositionEnum::values())
     ];
     $this->product->images()->createMany([
-        ['image' => 'fake1.jpg', 'position' => 'gallery'],
-        ['image' => 'fake2.jpg', 'position' => 'gallery'],
         ['image' => 'fake3.jpg', 'position' => 'gallery'],
         ['image' => 'fake4.jpg', 'position' => 'before'],
         ['image' => 'fake5.jpg', 'position' => 'after'],
     ]);
 
     $this->product->load('images', 'gallery', 'beforeImage', 'afterImage');
-    // dd($this->product->load([
-    //     // 'images',
-    //     'beforeImage',
-    //     'afterImage',
-    //     'gallery',
-    // ]));
 });
 
 test('user can view all product images (no pagination)', function () {
