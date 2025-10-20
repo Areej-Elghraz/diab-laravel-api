8d16a2927fcc11c6b9c9f94400f20c49dbfda636
Areej-Elghraz <areegelgharaz@gmail.com>
2025-10-21 02:29:04 +0300

    initial version deployment

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-define('LARAVEL_START', microtime(true));
+<body>
+    hello world
+</body>
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__ . '/storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
-require __DIR__ . '/vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__ . '/bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+</html>
