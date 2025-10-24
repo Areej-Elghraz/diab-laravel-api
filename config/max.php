f22cf283b3a1707dda7872797d3e977cfb442411
Areej-Elghraz <areegelgharaz@gmail.com>
2025-10-24 15:55:20 +0300

    update sensetive data

--- config/max.php
+++ config/max.php
@@ -3,5 +3,5 @@
 return [
   'admins' => env('MAX_ADMINS', 1),
   'banners' => env('MAX_BANNERS', 10),
-  'product_images' => env('MAX_PRODUCT_IMAGES', 5),
+  'product_images' => env('MAX_PRODUCT_IMAGES', 10),
 ];
